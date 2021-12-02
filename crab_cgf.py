d03cd1de013f76e1ecf51d632899cc58ace1385b
Horacio Crotte <horacio.crotte@gmail.com>
2021-12-02 20:57:46 +0100

    Fixed: config JobType maxMemoryMB

--- crab_cgf.py
+++ crab_cgf.py
@@ -13,8 +13,8 @@ date_up_to_minute = '_'.join(date_str.split(':')[0:2])
 
 
 fragment = 'PHSP_Photos'
-nEvents  = 100
-NJOBS    = 10
+nEvents  = 500
+NJOBS    = 4000
 
 step0    = "GS_"+fragment+"_step0.py" 
 step1    = "DR_"+fragment+"_step1.py"
@@ -26,7 +26,7 @@ GEN_file  = "GS_"+fragment+".root"
 MINI_file = "MINIAOD_"+fragment+"_step3.root" 
 
 
-config.General.requestName     = date_up_to_minute 
+config.General.requestName     = 'doubleCheck_'+fragment+date_up_to_minute 
 config.General.transferOutputs = True
 config.General.transferLogs    = False
 config.General.workArea        = fragment
@@ -40,17 +40,18 @@ config.JobType.inputFiles = [ step1, step2, step3, skim]
 config.JobType.disableAutomaticOutputCollection = True
 config.JobType.eventsPerLumi = 100
 config.JobType.numCores      = 1
-#config.JobType.maxMemoryMB  = 3500
+config.JobType.maxMemoryMB  = 3500
 config.JobType.scriptExe     = 'runMiniAOD.sh'
 config.JobType.scriptArgs   = ['fragment='+fragment]
-config.JobType.outputFiles   = [GEN_file, MINI_file]
+config.JobType.outputFiles   = [MINI_file]
+#config.JobType.outputFiles   = [GEN_file, MINI_file]
 
 
-config.Data.outputPrimaryDataset = fragment+'_'+date_up_to_minute
+config.Data.outputPrimaryDataset = fragment+date_up_to_minute
 config.Data.splitting = 'EventBased'
 config.Data.unitsPerJob = nEvents
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 #config.Data.outLFNDirBase = '/store/user/hcrottel/'
 config.Data.publication = False
 
-config.Site.storageSite = 'T3_CH_CERNBOX'
+config.Site.storageSite = 'T3_MX_Cinvestav'
