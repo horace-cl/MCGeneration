0c7e7136c6e185d0b65c5e79778ba6117f133a1f
Horacio Crotte <horacio.crotte@gmail.com>
2022-05-26 21:49:47 +0200

    Initizalize and compiling script for the GEN tuples production

---
+++ compileAnalyzer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export SCRAM_ARCH=slc7_amd64_gcc700
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+if [ -r CMSSW_10_2_20_UL/src ] ; then
+  echo release CMSSW_10_2_20_UL already exists
+else
+  scram p CMSSW CMSSW_10_2_20_UL
+fi
+
+cd CMSSW_10_2_20_UL/src
+eval `scram runtime -sh`
+
+mkdir -p Analyzer/MCanalyzer/plugins/
+cp ../../BuildFile.xml Analyzer/MCanalyzer/plugins/BuildFile.xml
+cp ../../Analyzer.cc Analyzer/MCanalyzer/plugins/Analyzer.cc
+cp ../../AnalyzerMiniAOD.cc Analyzer/MCanalyzer/plugins/AnalyzerMiniAOD.cc
+
+scram b
