248047a8188d80d4c6d5f20f5a5344c8416e449d
Horacio Crotte <horacio.crotte@gmail.com>
2024-11-29 08:24:29 +0100

    preparing script for BPix

--- Run3/Y2023/BPix/prepareMiniAOD_BPix.sh
+++ Run3/Y2023/BPix/prepareMiniAOD_BPix.sh
@@ -30,7 +30,7 @@ eval `scram runtime -sh`
 
 #Here we are going to place the fragment in an appropiate dir
 mkdir -p Configuration/GenProduction/python/
-cp ../../Fragments/${fragment}.py Configuration/GenProduction/python/${fragment}.py
+cp ../../../../Fragments/${fragment}.py Configuration/GenProduction/python/${fragment}.py
 #
 # #The same for the EDAnalyzer and the .xml file
 # mkdir -p Analyzer/MCanalyzer/plugins/
@@ -46,7 +46,7 @@ cd ../../
 
 #Produce the python cfg  with `cmsDriver` utility
 #Look at all flags
-cmsDriver.py Configuration/GenProduction/python/${fragment}.py --python_filename GS_${fragment}_step0.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:GS_${fragment}_step0.root --conditions 130X_mcRun3_2023_realistic_postBPix_v2 --beamspot Realistic25ns13p6TeVEarly2023Collision --step GEN,SIM --geometry DB:Extended --era Run3_2023 --no_exec --mc -n $events
+cmsDriver.py Configuration/GenProduction/python/${fragment}.py --python_filename GS_${fragment}_step0.py --eventcontent RAWSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --fileout file:GS_${fragment}_step0.root --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --beamspot Realistic25ns13p6TeVEarly2023Collision --step GEN,SIM --geometry DB:Extended --era Run3_2023 --no_exec --mc -n $events
 sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper \nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()"  GS_${fragment}_step0.py
 #cmsRun GS_${fragment}_step0.py
 #cmsRun mc_analyzer_cfg.py out=GS_${fragment} input=GS_${fragment}_step0 
@@ -94,17 +94,17 @@ cd ../..
 
 # SIM-RAW
 # cmsDriver command
-cmsDriver.py  --python_filename DR_${fragment}_step1.py --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --fileout file:DR_${fragment}_step1.root --pileup_input "dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23_130X_mcRun3_2023_realistic_v13-v1/PREMIX" --conditions 130X_mcRun3_2023_realistic_v14 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2023v12 --procModifiers premix_stage2 --geometry DB:Extended --filein file:GS_${fragment}_step0.root --datamix PreMix --era Run3_2023 --no_exec --mc -n -1
+cmsDriver.py  --python_filename DR_${fragment}_step1.py --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --fileout file:DR_${fragment}_step1.root --pileup_input "dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer23BPix_130X_mcRun3_2023_realistic_postBPix_v1-v1/PREMIX" --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2023v12    --procModifiers premix_stage2 --geometry DB:Extended --filein file:GS_${fragment}_step0.root --datamix PreMix --era Run3_2023 --no_exec --mc -n -1
 sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper\nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()" DR_${fragment}_step1.py
 
 # AOD
 # cmsDriver command
-cmsDriver.py  --python_filename AOD_${fragment}_step2.py --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --fileout file:AOD_${fragment}_step2.root --conditions 130X_mcRun3_2023_realistic_v14 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --filein file:DR_${fragment}_step1.root --era Run3_2023 --no_exec --mc -n -1
+cmsDriver.py  --python_filename AOD_${fragment}_step2.py --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --fileout file:AOD_${fragment}_step2.root --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step RAW2DIGI,L1Reco,RECO,RECOSIM --geometry DB:Extended --filein file:DR_${fragment}_step1.root --era Run3_2023 --no_exec --mc -n -1
 sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper\nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()"  AOD_${fragment}_step2.py
 
 # MINIAOD
 # cmsDriver command
-cmsDriver.py  --python_filename MINIAOD_${fragment}_step3.py --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --fileout file:MINIAOD_${fragment}_step3.root --conditions 130X_mcRun3_2023_realistic_v14 --step PAT --geometry DB:Extended --filein file:AOD_${fragment}_step2.root --era Run3_2023 --no_exec --mc -n -1
+cmsDriver.py  --python_filename MINIAOD_${fragment}_step3.py --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --fileout file:MINIAOD_${fragment}_step3.root --conditions 130X_mcRun3_2023_realistic_postBPix_v6 --step PAT --geometry DB:Extended --filein file:AOD_${fragment}_step2.root --era Run3_2023 --no_exec --mc -n -1
 sed -i "20 a from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper\nrandSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)\nrandSvc.populate()"  MINIAOD_${fragment}_step3.py
 
 
